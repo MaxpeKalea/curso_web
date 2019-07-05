69f15d1f64206eb3ac7d87b7702075a889e2e603
Pikachu <pikapik@chu.es>
2019-07-05 21:15:09 +0200

    05 07 clase

---
+++ JS_OOP/16_constructoras.js
@@ -0,0 +1,24 @@
+// 19:30 05 07
+
+// Objeto JSON o literal
+
+const p1 = {
+        nombre : 'Pepe',
+        apellidos : 'Pérez',
+        edad : 20,
+        altura : 178,
+        isMujer : false,
+        formacion : 'Ingeniero',
+        aficiones : ['Leer','bailar','programar JS'] 
+}
+
+// Prototipos JS (en JS no hay clases)
+
+// Función constructora:
+
+function persona() {}
+
+
+// 20:30 Prototipo o prototype // importante
+
+
