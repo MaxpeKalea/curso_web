1f479df6b7be7cec0581bdd110518e21eb25bf4c
Pikachu <pikapik@chu.es>
2019-09-11 21:19:08 +0200

    php

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Inicio PHP</title>
+</head>
+<?php
+
+    $titulo="Hola mundo"
+
+?>
+<body>
+    <header>
+        <h1><?= $titulo ?></h1>
+    </header>
+</body>
+<?php
+
+    $titulo="Otro título"
+
+?>
+<section>
+    <h2><?= $titulo ?></h2>
+</section>
+</html>
